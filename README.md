37e3763b4233d122e079d02416bc6e81091ca823
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-03 14:42:21 +0530

    Sample

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # terraform-vpc
+
+This is the root module to created VPC on AWS Cloud and this root module looks for the terraformn modules code that were made and availble on the top of the backend-module
+
+`https://github.com/jogendra-b54/tf-module-vpc.git`
+
+
+## Terrafile :
+
+1> this is is a binary written in GO
+2> All it does is, it downloads the backend module from the mentioned branch and keeps it's locally available to the root module 
+3> So that your intended backend module can be cloned from the branch of your choice
+
---
+++ main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git"
+}
+
+// You cannot variablize this piece of code in the source parameter
+
+/*
+module "vpc" {
+  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git?ref=v1.2.0"  //you can mentione which Tag  you can pull
+}
+*/
+
+/*
+module "vpc" {
+  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git?ref=514d3435gtrf44"  //you can also mention which commit you can pull
+}
+*/
+
+/*
+module "vpc" {
+  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git@dev"   //you can also mention which branch you can pull
+}
+*/
