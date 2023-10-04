7289cdf90ef5b09ea0e4f5ec7f9d259e0214cca4
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-04 11:30:44 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git"
+  source       = "./vendor/modules/vpc"
+  VPC_CIDR     = var.VPC_CIDR
  }
 
 // You cannot variablize this piece of code in the source parameter
