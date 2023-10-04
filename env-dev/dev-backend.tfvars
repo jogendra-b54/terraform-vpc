b93ca44842b28b197cf09c303af5c8953321647f
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-04 11:36:00 +0530

    Auto push

--- env-dev/dev-backend.tfvars
+++ env-dev/dev-backend.tfvars
@@ -1,3 +1,3 @@
- bucket = "b54-terraform-remote-state"
- key    = "vpc/dev/terraform.tfstate"
- region = "us-east-1"
+ bucket          = "b54-terraform-remote-state"
+ key             = "vpc/dev/terraform.tfstate"
+ region          = "us-east-1"
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source       = "./vendor/modules/vpc"
-  VPC_CIDR     = var.VPC_CIDR
+  source          = "./vendor/modules/vpc"
+  VPC_CIDR        = var.VPC_CIDR
  }
 
 // You cannot variablize this piece of code in the source parameter
