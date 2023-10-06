8849cf48e9d26e95f771a98527e5cbdf30b80700
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-06 17:27:55 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,25 +1,11 @@
 module "vpc" {
-  source          = "./vendor/modules/vpc"
-  VPC_CIDR        = var.VPC_CIDR
-  ENV             = var.ENV
- }
+  source           = "./vendor/modules/vpc"
+  VPC_CIDR         = var.VPC_CIDR
+  ENV              = var.ENV
+  AZ               = var.AZ
+  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+}
 
 // You cannot variablize this piece of code in the source parameter
 
-/*
-module "vpc" {
-  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git?ref=v1.2.0"  //you can mentione which Tag  you can pull
-}
-*/
-
-/*
-module "vpc" {
-  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git?ref=514d3435gtrf44"  //you can also mention which commit you can pull
-}
-*/
-
-/*
-module "vpc" {
-  source = "git::https://github.com/jogendra-b54/tf-module-vpc.git@dev"   //you can also mention which branch you can pull
-}
-*/
--- vars.tf
+++ vars.tf
@@ -1,4 +1,5 @@
 variable "VPC_CIDR" {}
-variable "ENV" {
-  
-}
+variable "ENV" {}
+variable "AZ" {}
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
