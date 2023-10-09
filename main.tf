d705b703c5fd9c6238c6c76c265b2fc418202a54
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-09 22:24:17 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
-  source           = "./vendor/modules/vpc"
-  VPC_CIDR         = var.VPC_CIDR
-  ENV              = var.ENV
-  AZ               = var.AZ
-  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+  source              = "./vendor/modules/vpc"
+  VPC_CIDR            = var.VPC_CIDR
+  ENV                 = var.ENV
+  AZ                  = var.AZ
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  DEFAULT_VPC_ID       = var.DEFAULT_VPC_ID 
+  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID 
+  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
 }
 
 // You cannot variablize this piece of code in the source parameter
