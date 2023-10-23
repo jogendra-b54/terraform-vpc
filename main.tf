1f5fec690fd6c917c7323a2477c55ec28226f280
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-23 16:10:13 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 module "vpc" {
-  source              = "./vendor/modules/vpc"
-  VPC_CIDR            = var.VPC_CIDR
-  ENV                 = var.ENV
-  AZ                  = var.AZ
-  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID 
-  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT      = var.DEFAULT_VPC_RT
+  source                    = "./vendor/modules/vpc"
+  VPC_CIDR                  = var.VPC_CIDR
+  ENV                       = var.ENV
+  AZ                        = var.AZ
+  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_ID            = var.DEFAULT_VPC_ID 
+  DEFAULT_VPC_CIDR          = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT            = var.DEFAULT_VPC_RT
+  PRIVATE_HOSTED_ZONE_NAME  = var.PRIVATE_HOSTED_ZONE_NAME
+  PRIVATE_HOSTED_ZONE_ID    = var.PRIVATE_HOSTED_ZONE_ID
+  PUBLIC_HOSTED_ZONE_NAME   = var.PUBLIC_HOSTED_ZONE_NAME
+  PUBLIC_HOSTED_ZONE_ID     = var.PUBLIC_HOSTED_ZONE_ID
+
 }
 
 // You cannot variablize this piece of code in the source parameter
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -1,5 +1,11 @@
 AZ                  =   [ "us-east-1a" , "us-east-1b" ]
 
-DEFAULT_VPC_ID      = "vpc-0c191255a724f193a"
-DEFAULT_VPC_CIDR    = "172.31.0.0/16"
-DEFAULT_VPC_RT      =  "rtb-0d3e4cc4823b7267e"
+DEFAULT_VPC_ID                = "vpc-0c191255a724f193a"
+DEFAULT_VPC_CIDR              = "172.31.0.0/16"
+DEFAULT_VPC_RT                =  "rtb-0d3e4cc4823b7267e"
+
+PRIVATE_HOSTED_ZONE_NAME      = "roboshop.internal"
+PRIVATE_HOSTED_ZONE_ID        =  "Z08104102TDJJFARM8HPK"
+
+PUBLIC_HOSTED_ZONE_NAME       = "b54-roboshop.in"
+PUBLIC_HOSTED_ZONE_ID         = "Z00558466PWU6BGNPYQG"
